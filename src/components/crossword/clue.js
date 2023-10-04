78719014ea95a0ebfd094b205a935e58826eb4c8
jaredgastelum <92835110+jaredgastelum@users.noreply.github.com>
2023-10-04 10:51:48 -0600

    Console.log when double clicked

--- src/components/crossword/clue.js
+++ src/components/crossword/clue.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Modal } from "@mui/material";
 import Person from "@/components/person";
 import { useUser } from "@/contexts/UserContext";
@@ -6,7 +6,7 @@ import { useUser } from "@/contexts/UserContext";
 function Clue(props) {
   const { number, word, clue } = props;
   const [displayClue, setDisplayClue] = useState(true);
-  const [showPersonInfo, setShowPersonInfo] = useState(false); 
+  const [showPersonInfo, setShowPersonInfo] = useState(false);
   const [currentPerson, setCurrentPerson] = useState(null);
   const { userFSData } = useUser();
 
@@ -16,12 +16,22 @@ function Clue(props) {
     setDisplayClue(!displayClue);
   }
 
+  // Highlights the row or column in the crossword when the corresponding clue is clicked
+  function handleClueClick() {
+    console.log("clue " + number + " clicked");
+  }
+
   // Shows the person Modal when their name is clicked (little convoluted, maybe fix later)
-  function handleNameClick() { 
+  function handleNameClick() {
     if (!displayClue) {
-      const transformedMap = new Map([...userFSData.entries()].map(([key, value]) => [value.name.compressedName, { key }]));
-      const foundPerson = transformedMap.get(word)
-      const realFoundPerson = userFSData.get(Object.values(foundPerson)[0])
+      const transformedMap = new Map(
+        [...userFSData.entries()].map(([key, value]) => [
+          value.name.compressedName,
+          { key },
+        ])
+      );
+      const foundPerson = transformedMap.get(word);
+      const realFoundPerson = userFSData.get(Object.values(foundPerson)[0]);
       if (foundPerson) {
         setCurrentPerson(realFoundPerson);
         setShowPersonInfo(true);
@@ -35,14 +45,15 @@ function Clue(props) {
 
   return (
     <>
-      <div 
+      <div
         onContextMenu={handleContextMenu}
         onClick={displayClue ? null : handleNameClick}
+        onDoubleClick={handleClueClick}
       >
         {number + ". " + (displayClue ? clue : word)}
       </div>
       <Modal open={showPersonInfo} onClose={() => setShowPersonInfo(false)}>
-        <Person personData={currentPerson}/>
+        <Person personData={currentPerson} />
       </Modal>
     </>
   );
